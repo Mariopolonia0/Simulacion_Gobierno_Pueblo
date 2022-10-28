11ea5095409b4f275251e4c2a53aab7ba173cb1b
Mario <mariopolonia0@gmail.com>
2022-10-27 21:30:53 -0400

    agregando ejecicio de frio frio

---
+++ app/src/main/java/com/duramas/simulaciongobiernopueblo/frio_frio/ActivityFrioFrio.kt
@@ -0,0 +1,118 @@
+package com.duramas.simulaciongobiernopueblo.frio_frio
+
+import android.opengl.Visibility
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import com.duramas.simulaciongobiernopueblo.R
+import com.duramas.simulaciongobiernopueblo.databinding.ActivityFrioFrioBinding
+import com.duramas.simulaciongobiernopueblo.databinding.FragmentMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+/*
+    descripcion de cubo
+    ----------------------------
+    +cubo es de 5 litro = 5000 ml  coje 6 litro
+    -cuando el cubo se acabe termina la simulacion
+
+
+    tipos de vaso
+    ------------------------------
+    -vaso grande  $90,  3.15
+    -vaso mediano $50, 2.1
+    -vaso pequeño $25  1.05
+
+    descripcion de vaso
+    -----------------------------------
+    vaso pequeño
+    2 ml  jugo limon
+    2 oz azucar
+    3 cubitos de hielos
+    3 minutos
+
+    vaso mediano (se multiplica lo del el vaso pequeño por 2.1)
+    4.2 ml jugo limon
+    4.2 oz azucar
+    6 cubitos de hielos
+
+    vaso grande (se multiplica lo del el vaso pequeño por 3.15)
+    6.3 ml jugo limon
+    6.3 oz azucar
+    9 cubitos de hielos
+
+    imprimir en ejecucion
+    -----------------------
+    -persona que llegan
+    -persona de la que llegan preguntan
+    -persona que compran
+    -frozen comprado
+    -propina de 10 peso dejada por el baso grande
+
+    imprimir fin de programa
+    --------------------------------
+    -benefio
+    -costo totales
+    -ventas
+    -propinas
+
+ */
+
+
+class ActivityFrioFrio : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFrioFrioBinding
+
+    var listaVaso = arrayListOf<Vaso>(
+        Vaso(2.0, 2.0, 3),
+        Vaso(4.2, 4.2, 6),
+        Vaso(6.3, 6.3, 9),
+    )
+
+    var cubo = 2000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFrioFrioBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.play -> {
+                Snackbar.make(this.binding.root,"Inicio la Simulacion",Snackbar.LENGTH_LONG)
+
+                calcularIngredienteVaso()
+                true
+            }
+            R.id.stop -> {
+                binding.progressBar.visibility = View.INVISIBLE
+                Snackbar.make(binding.root,"Termino la Simulacion",Snackbar.LENGTH_LONG)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun calcularIngredienteVaso(){
+        binding.progressBar.visibility = View.VISIBLE
+
+        while (cubo > 0){
+            binding.porcientoCuboTextView.setText("Mario")
+            println(cubo.toString())
+            cubo --
+            //Thread.sleep(200)
+        }
+        binding.progressBar.visibility = View.INVISIBLE
+    }
+
+}
---
+++ app/src/main/java/com/duramas/simulaciongobiernopueblo/frio_frio/vaso.kt
@@ -0,0 +1,15 @@
+package com.duramas.simulaciongobiernopueblo.frio_frio
+
+
+//vaso pequeño
+//2 ml  jugo limon
+//2 oz azucar
+//3 cubitos de hielos
+//3 minutos
+
+data class Vaso(
+    val mlJugo: Double = 0.0,
+    val ozAzuca: Double = 0.0,
+    val cuvoHielo: Int = 0
+)
+
